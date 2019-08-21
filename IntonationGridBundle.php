1218a47768b48c541f2c650a7474eacf571dea93
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2019-08-21 23:06:11 +0200

    Create IntonationGridBundle

---
+++ IntonationGridBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the IntonationGridBundle.
+ *
+ * (c) Wolfgang Drescher <drescher.wolfgang@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Intonation\GridBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IntonationGridBundle extends Bundle
+{
+}
